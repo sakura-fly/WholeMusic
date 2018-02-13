72c536381d2b10bccdc4cbd0b1592c1c2526807d
eggfly <eggfly@qq.com>
2018-02-13 12:13:38 +0800

    remove unused expcetion
    Signed-off-by: eggfly <eggfly@qq.com>

--- src/main/java/wholemusic/core/Main.java
+++ src/main/java/wholemusic/core/Main.java
@@ -19,7 +19,7 @@ public class Main {
         }
     };
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         testNeteaseAlbum();
     }
 
