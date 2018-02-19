ceae0460a55633c7e5cec5980c4bfdf1b74a1367
eggfly <eggfly@qq.com>
2018-02-20 01:57:11 +0800

    TextUtils
    Signed-off-by: eggfly <eggfly@qq.com>

--- src/main/java/wholemusic/core/util/TextUtils.java
+++ src/main/java/wholemusic/core/util/TextUtils.java
@@ -3,6 +3,12 @@ package wholemusic.core.util;
 import java.util.Iterator;
 
 public class TextUtils {
+    /**
+     * Returns a string containing the tokens joined by delimiters.
+     *
+     * @param tokens an array objects to be joined. Strings will be formed from
+     *               the objects by calling object.toString().
+     */
     public static String join(CharSequence delimiter, Iterable tokens) {
         StringBuilder sb = new StringBuilder();
         Iterator<?> it = tokens.iterator();
@@ -14,5 +20,18 @@ public class TextUtils {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Returns true if the string is null or 0-length.
+     *
+     * @param str the string to be examined
+     * @return true if str is null or zero length
+     */
+    public static boolean isEmpty(CharSequence str) {
+        if (str == null || str.length() == 0)
+            return true;
+        else
+            return false;
     }
 }
